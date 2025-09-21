221b414e019491efa13c222d7bd0d5aa7007ddfd
llbob <miniporse@gmail.com>
2025-09-21 12:46:51 +0200

    Fix build issue

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -5,6 +5,9 @@ export function Footer() {
   return (
     <footer className="bg-background">
       <Container>
+        <div className="py-6 text-center">
+          <p className="text-sm">© {new Date().getFullYear()} memaelade.lifestyle</p>
+        </div>
       </Container>
     </footer>
   );
